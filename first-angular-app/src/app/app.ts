61094c4e3b0698f0ddce06e12b3c7b8ef0898a87
VishnuBhangale <bhangale.vishnu7@gmail.com>
2025-12-09 07:02:40 +0530

    Saparate component code
    added task component and show the task

--- first-angular-app/src/app/app.ts
+++ first-angular-app/src/app/app.ts
@@ -3,18 +3,25 @@ import { RouterOutlet } from '@angular/router';
 import { User } from './user/user';
 import { Header } from './header/header';
 import { DUMMY_USERS } from './user/dumy-users';
+import { Tasks } from "./tasks/tasks";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,User,Header],
+  imports: [User, Header, Tasks],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
 user=DUMMY_USERS;
+selectedUserId?:string;;
+get selectedUser(){
+  return this.user.find((user)=>user.id===this.selectedUserId)!;
+}
 onSelectUser(id:string)
 {
   console.log('selected user id:'+id);
+  this.selectedUserId=id;
+
 }
 
 }
--- first-angular-app/src/app/user/user.ts
+++ first-angular-app/src/app/user/user.ts
@@ -1,22 +1,39 @@
+import { Interpolation } from '@angular/compiler';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-
+// type users={
+//   id: string;
+//     name: string;
+//    avatar: string;
+// }
+interface Users{
+     id: string;
+    name: string;
+    avatar: string;
+}
 @Component({
   selector: 'app-user',
   templateUrl: './user.html',
   styleUrls: ['./user.css'],
 })
 export class User {
-  @Input() id!: string;
-  @Input() name!: string;
-  @Input() avatar!: string;
+  // @Input() id!: string;
+  // @Input() name!: string;
+  // @Input() avatar!: string;
+  // @Input() user! : {
+  //   id: string;
+  //   name: string;
+  //   avatar: string;
+  // }
+  @Input() user!: Users;
+  
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectedUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 
 }
