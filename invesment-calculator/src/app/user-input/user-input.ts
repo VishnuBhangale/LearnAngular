af47297f72ab9799d828cf3341dcad543f543780
VishnuBhangale <bhangale.vishnu7@gmail.com>
2025-12-17 06:53:21 +0530

    Create New Project for Invesment calculator
    Create New Project for Invesment calculator

---
+++ invesment-calculator/src/app/user-input/user-input.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-input',
+  imports: [FormsModule],
+  templateUrl: './user-input.html',
+  styleUrl: './user-input.css',
+})
+export class UserInput {
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';  
+onSubmit() {
+
+}
+
+}
