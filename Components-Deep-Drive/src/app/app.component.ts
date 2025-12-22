ed0cfb2ced7d0bab9691f57b51302412ba445d29
VishnuBhangale <bhangale.vishnu7@gmail.com>
2025-12-22 14:58:07 +0530

    common code in one component

--- Components-Deep-Drive/src/app/app.component.ts
+++ Components-Deep-Drive/src/app/app.component.ts
@@ -3,12 +3,13 @@ import { Header } from './header/header';
 import { ServerStatus } from './dashboard/server-status/server-status';
 import { Traffic } from './dashboard/traffic/traffic';
 import { Tickets } from './dashboard/tickets/tickets';
+import { DashboardItemComponent } from './dashboard/dashboard-item/dashboard-item.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [Header,ServerStatus,Traffic,Tickets],
+  imports: [Header, ServerStatus, Traffic, Tickets,DashboardItemComponent],
 })
 export class AppComponent {
   
