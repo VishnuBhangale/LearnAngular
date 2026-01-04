58bbfac949a022c95aef13a36179ea8bf9aa1939
VishnuBhangale <bhangale.vishnu7@gmail.com>
2026-01-04 07:06:58 +0530

    new ticket

--- Components-Deep-Drive/src/app/dashboard/tickets/tickets.ts
+++ Components-Deep-Drive/src/app/dashboard/tickets/tickets.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { NewTicketComponent } from "./new-ticket/new-ticket.component";
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
   templateUrl: './tickets.html',
   styleUrl: './tickets.css',
+  imports: [NewTicketComponent],
 })
 export class Tickets {
 
